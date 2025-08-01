e9775e351e2d0086be6eb0ccd82457981d7d5e77
Enzo Vieira <enzovieira16@hotmail.com>
2025-08-01 13:53:37 -0300

    style(header): add basic styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,26 @@
-import { LogoutButton } from "@/components/logout-button"
+import { Gamepad2 } from "lucide-react"
+
+import { LogoutButton } from "./logout-button"
 
 export function Header() {
   return (
-    <header>
-      <h1>Steam Roulette</h1>
-      <p>Find your next adventure</p>
+    <header className="p-4 border-b flex items-center justify-between">
+      <div className="flex gap-3.5 items-center">
+        <div className="size-12 rounded-sm flex items-center justify-center bg-red-500">
+          <Gamepad2 className="size-8 text-white" />
+        </div>
+
+        <div>
+          <h1 className="text-2xl font-bold">Steam Roulette</h1>
+          <p className="text-muted-foreground text-sm font-medium">
+            Find your next adventure
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <LogoutButton />
+      </div>
     </header>
   )
 }
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -2,13 +2,17 @@
 
 import { signOut } from "next-auth/react"
 
+import { Button } from "./ui/button"
+import { LogOut } from "lucide-react"
+
 export function LogoutButton() {
   return (
-    <button
-      onClick={() => signOut({ redirect: true, callbackUrl: "/login" })}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => signOut({ callbackUrl: "/login" })}
     >
-      Login with Steam
-    </button>
+      <LogOut className="size-4" />
+    </Button>
   )
 }
