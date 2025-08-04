c51423539ba28e6339df72dd1e3c2479deeb2d53
Enzo Vieira <enzovieira16@hotmail.com>
2025-08-03 21:13:04 -0300

    style(login): add login page style

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,48 @@
 import { LoginButton } from "@/components/login-button"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Gamepad2 } from "lucide-react"
 
 export default function LoginPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Login</h1>
-      <LoginButton />
+    <main className="flex flex-col gap-6 min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <div className="mx-auto mb-6 size-12 rounded-sm flex items-center justify-center gradient">
+            <Gamepad2 className="size-8 text-white" />
+          </div>
+
+          <h1 className="text-2xl font-bold text-center">Steam Roulette</h1>
+          <p className="text-muted-foreground text-center text-sm">
+            Log in to access your Steam Library and discover your next gaming
+            adventure
+          </p>
+        </CardHeader>
+
+        <CardContent>
+          <LoginButton />
+        </CardContent>
+
+        <CardFooter className="flex-col gap-4">
+          <p className="text-xs text-muted-foreground text-center">
+            We use Steam OpenID to securely authenticate your account. Your
+            credentials are never stored.
+          </p>
+
+          <div className="flex gap-1.5 items-center">
+            <div className="size-2 rounded-full bg-blue-500"></div>
+            <p className="text-xs text-muted-foreground">
+              Secure Steam authentication
+            </p>
+          </div>
+        </CardFooter>
+      </Card>
+
+      <p className="text-center text-sm text-muted-foreground">
+        Don&apos;t have a Steam account?{" "}
+        <a href="https://store.steampowered.com/join/" className="underline">
+          Create one here
+        </a>
+      </p>
     </main>
   )
 }
--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -1,14 +1,18 @@
 "use client"
 
 import { signIn } from "next-auth/react"
+import { Button } from "./ui/button"
+import { LogIn } from "lucide-react"
 
 export function LoginButton() {
   return (
-    <button
-      onClick={() => signIn("steam", { redirect: true, callbackUrl: "/" })}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    <Button
+      className="gradient text-white w-full"
+      size="lg"
+      onClick={() => signIn("steam", { callbackUrl: "/" })}
     >
+      <LogIn className="mr-2 size-4" />
       Login with Steam
-    </button>
+    </Button>
   )
 }
